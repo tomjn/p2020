3b232fdd4021390e344fa6f2b6c84c2bf499ddac
annemirasol <annemirasol@d957f892-c61d-0410-b221-f235e6eecf30>
2020-04-10 12:59:17 +0000

    P2020 Theme Improvements
    Summary: This is a batch deploy of updates to the P2020 theme, maintained in https://github.com/Automattic/wp-for-teams.
    
    Test Plan: All test plans, code review and relevant documentation are maintained in https://github.com/Automattic/wp-for-teams.
    
    Differential Revision: https://code.a8c.com/D41696

--- sidebar.php
+++ sidebar.php
@@ -7,8 +7,65 @@
 
 if( ! is_active_sidebar( 'sidebar-1' ) )
 	return;
+
+$site_slug = \WPCOM_Masterbar::get_calypso_site_slug( get_current_blog_id() );
+
+$invite_url = 'https://wordpress.com/people/new/' . $site_slug;
+$customizer_url = 'https://wordpress.com/customize/' . $site_slug;
+$help_url = 'https://wordpress.com/help';
 ?>
-	<aside id="sidebar">
-		<?php do_action( 'before_sidebar' ); ?>
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+	<aside id="sidebar" class="p2020-sidebar">
+
+		<?php if ( has_header_image() ) : ?>
+			<img
+				src="<?php header_image(); ?>"
+				alt=""
+				class="p2020-sidebar__header-image"
+				width="<?php echo get_custom_header()->width; ?>"
+				height="<?php echo get_custom_header()->height; ?>"
+			>
+		<?php endif; ?>
+
+		<div class="p2020-sidebar__inner-main">
+
+			<?php do_action( 'before_sidebar' ); ?>
+			<?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+			<footer class="p2020-sidebar__footer">
+				<ul class="p2020-sidebar__footer-links">
+					<?php if ( current_user_can( 'create_users' ) ) : ?>
+						<li>
+							<a href="<?php echo esc_url( $invite_url ) ?>">
+								<?php _e( 'Invite people to your team', 'p2020' ); ?>
+							</a>
+						</li>
+					<?php endif; ?>
+					<?php if ( current_user_can( 'customize' ) ) : ?>
+					<li>
+						<a href="<?php echo esc_url( $customizer_url ) ?>">
+							<?php _e( 'Customize your site', 'p2020' ); ?>
+						</a>
+					</li>
+					<?php endif; ?>
+					<li>
+						<?php
+							// CAUTION: This class is a fragile hack to get O2 to attach its
+							// cheatsheet toggling event listener to this element.
+							// See https://opengrok.a8c.com/source/xref/trunk/wp-content/plugins/p2tenberg/p2/js/keyboard.js#35
+						?>
+						<button class="o2-toggle-keyboard-help">
+							<?php _e( 'Keyboard shortcuts', 'p2020' ); ?>
+						</button>
+					</li>
+					<li>
+						<a href="<?php echo esc_url( $help_url ) ?>">
+							<?php _e( 'Help', 'p2020' ); ?>
+						</a>
+					</li>
+				</ul>
+			</footer>
+
+		</div><!-- .p2020-sidebar__inner-main -->
+
 	</aside>
