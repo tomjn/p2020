4f876e083aa4c9155e679946a0581d393952ba78
annemirasol <annemirasol@d957f892-c61d-0410-b221-f235e6eecf30>
2020-05-01 11:34:44 +0000

    P2020 Theme Improvements
    Summary: This is a batch deploy of updates to the P2020 theme, maintained in https://github.com/Automattic/wp-for-teams.
    
    Test Plan: All test plans and relevant documentation are maintained in https://github.com/Automattic/wp-for-teams.
    
    Differential Revision: https://code.a8c.com/D42793

--- p2020/header.php
+++ p2020/header.php
@@ -15,7 +15,7 @@
 <meta http-equiv="cleartype" content="on">
 
 <?php // Will be overwritten with git commit hash on `gulp build` (see gulpfile) ?>
-<meta name="p2020-build" revision="8ef4b08">
+<meta name="p2020-build" revision="7c963a2">
 
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
