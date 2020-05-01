4a940681697240a48b1fd690e10cf6a3c607386f
annemirasol <annemirasol@d957f892-c61d-0410-b221-f235e6eecf30>
2020-05-01 11:34:44 +0000

    P2020 Theme Improvements
    Summary: This is a batch deploy of updates to the P2020 theme, maintained in https://github.com/Automattic/wp-for-teams.
    
    Test Plan: All test plans and relevant documentation are maintained in https://github.com/Automattic/wp-for-teams.
    
    Differential Revision: https://code.a8c.com/D42793

--- widgets/filter/js/read-more.js
+++ widgets/filter/js/read-more.js
@@ -1,27 +1,25 @@
 ( function( $ ) {
-	const trimComment = ( commentContent ) => {
-		$( commentContent )
+	const trimComment = ( $commentContent ) => {
+		$commentContent
 			.children( ':first-child' )
 			.addClass( 'p2020-comment-read-more-excerpt' );
-		$( commentContent )
+		$commentContent
 			.children( ':not(.o2-comment-actions):nth-child(n+2)' )
 			.hide();
 	};
 
-	const untrimComment = ( commentContent ) => {
-		$( commentContent )
+	const untrimComment = ( $commentContent ) => {
+		$commentContent
 			.children()
 			.removeClass( 'p2020-comment-read-more-excerpt' );
-		$( commentContent )
-			.children()
-			.show();
+		$commentContent.children().show();
 	};
 
-	const shouldTrimComment = ( commentContent ) => {
-		const trueHeight = $( commentContent ).get( 0 ).scrollHeight;
-		trimComment( $( commentContent ) );
-		const trimmedHeight = $( commentContent ).get( 0 ).scrollHeight;
-		untrimComment( $( commentContent ) );
+	const shouldTrimComment = ( $commentContent ) => {
+		const trueHeight = $commentContent.get( 0 ).scrollHeight;
+		trimComment( $commentContent );
+		const trimmedHeight = $commentContent.get( 0 ).scrollHeight;
+		untrimComment( $commentContent );
 
 		return trueHeight > trimmedHeight;
 	};
@@ -41,8 +39,8 @@
 		$( '.o2-display-comments-toggle' ).hide();
 	};
 
-	const addReadMoreToComment = ( commentContent ) => {
-		$( commentContent ).prepend(
+	const addReadMoreToComment = ( $commentContent ) => {
+		$commentContent.prepend(
 			'<div class="p2020-comment-read-more-mask">' +
 				'<a href="/" class="p2020-comment-read-more-trigger">' +
 				window.p2020FilterReadMore.readComment +
@@ -60,10 +58,80 @@
 		} );
 	};
 
+	const summarizeCommentThread = ( $commentThread, $summary ) => {
+		const commentsCount = 1 + $commentThread.find( '.o2-comment' ).length;
+		const avatarsHtml = $( '<div/>' )
+			.append(
+				$commentThread
+					.find( '.avatar' )
+					.clone()
+					.removeClass( 'grav-hijack' )
+			)
+			.html();
+
+		// Update count
+		const currentCount =
+			parseInt(
+				$summary.find( '.p2020-comments-summary-count > .count' ).html()
+			) || 0;
+		$summary
+			.find( '.p2020-comments-summary-count' )
+			.html(
+				`<span class="count">${ currentCount + commentsCount }</span> ${
+					window.p2020FilterReadMore.moreComments
+				}`
+			);
+
+		// Update avatars
+		$summary
+			.find( '.p2020-comments-summary-avatars' )
+			.append( avatarsHtml );
+	};
+
+	const insertSummaryContainer = ( insertBeforeElement ) => {
+		const summary = $(
+			'<button class="p2020-comments-summary">' +
+				'<span class="p2020-comments-summary-count"></span>' +
+				'<span class="p2020-comments-summary-avatars"></span>' +
+				'</button>'
+		);
+		summary.insertBefore( insertBeforeElement );
+
+		return summary;
+	};
+
+	const getSummaryContainer = ( $commentThread ) => {
+		const $summary = $commentThread
+			.prevUntil( '.p2020-active-thread' )
+			.filter( '.p2020-comments-summary' )
+			.first();
+		if ( ! $summary.html() ) {
+			return insertSummaryContainer( $commentThread );
+		}
+
+		return $summary;
+	};
+
+	const collapseCommentThreads = () => {
+		$( '.o2-post-comments > .o2-comment' ).each( function() {
+			const hasUnreadComment =
+				$( this ).hasClass( 'p2020-unread-comment' ) ||
+				$( this ).find( '.p2020-unread-comment' ).length > 0;
+			if ( ! hasUnreadComment ) {
+				const summary = getSummaryContainer( $( this ) );
+				summarizeCommentThread( $( this ), summary );
+				$( this ).addClass( 'p2020-inactive-thread' );
+			} else {
+				$( this ).addClass( 'p2020-active-thread' );
+			}
+		} );
+	};
+
 	$( document ).ready( function() {
 		addReadMoreToPosts();
 		forceDisplayComments();
 		trimSeenComments();
+		collapseCommentThreads();
 	} );
 
 	$( document.body ).on(
@@ -92,4 +160,14 @@
 			return false;
 		}
 	);
+
+	$( document.body ).on( 'click', '.p2020-comments-summary', function() {
+		$( this )
+			.nextUntil( '.p2020-active-thread' )
+			.removeClass( 'p2020-inactive-thread' );
+
+		$( this ).hide();
+
+		return false;
+	} );
 } )( jQuery );
