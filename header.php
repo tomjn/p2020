fa631fb181015ee27bbfc85a64b873387984bfc9
0mirka00 <0mirka00@d957f892-c61d-0410-b221-f235e6eecf30>
2020-07-24 18:08:15 +0000

    P2020 Theme Improvements
    Summary: Batch deploy of updates to the P2020 theme, maintained in https://github.com/Automattic/p2.
    
    Test Plan: Batch deploy of updates to the P2020 theme, maintained in https://github.com/Automattic/p2.
    
    Differential Revision: https://code.a8c.com/D46876

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <meta http-equiv="cleartype" content="on">
 
 <?php // Will be overwritten with git commit hash on `gulp build` (see gulpfile) ?>
-<meta name="p2020-build" revision="04e57de">
+<meta name="p2020-build" revision="cf61fb6">
 
 <title><?php wp_title( '–', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
